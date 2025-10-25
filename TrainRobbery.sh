9fc91303c91747fab856a776f22cbc2acaa3960c
hidethecyanide <128944102+hidethecyanide@users.noreply.github.com>
2025-10-25 14:23:10 -0500

    Add files via upload

---
+++ TrainRobbery.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo -e "Well, this is the big one. One last job.\n"
+sleep 3
+echo -e "One train robbery and we can all retire...maybe.\n "
+sleep 2
+echo -e "Its going to be dangerous though, this job will test all of our intruding and cracking skills.\nWe are going to have to review all of the stages of the instusion,\nas I always have a plan.\n\nThe safe with all of the gold will be locked with 3 4-digit numbers scattered\naround different rooms of the train, so lets get to it."
+
+sleep 10
+
+echo -e "\n\n\nThe numbers you find may be located in a chest, but you can use different commands to find them\nIt is one thing to get into the room, but another to find it,\nand they will only get harder to look for, but dont worry, I will help."
+
+sleep 10
+
+echo -e "\n\n\nSelect an option below to begin review of the different rooms of the train,\nand different instusions you can implement is this heist.\nIf you ever need help, select the help option, but other than that, just review
+the instrusion plan and then we will move on to the BIG ONE.\n"
+sleep 10
+
+
