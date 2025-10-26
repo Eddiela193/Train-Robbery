c708ff89b841afabefe9ba7869da63b0ef783bc4
hidethecyanide <128944102+hidethecyanide@users.noreply.github.com>
2025-10-25 23:25:58 -0500

    Create keys.sh

---
+++ theBigOne/keys.sh
@@ -0,0 +1,18 @@
+echo -e "The keys you are looking for unlock the vault
+at the end of the train, and the order does not matter.\n\n"
+
+echo -e "The keys are always in a file naked keyx with
+x being the cart number , ex. buffer overflow has key2.\n\n"
+
+sleep 2
+
+echo -e "However, sometimes the file is hidden, and sometimes
+it is hidden in text, but it is always 4 digits and findable in
+one command. For example, a txt file containing only letters will
+have the key, so you can search for the presence of any
+number and find it. THe key will always be in the folder
+you have access to when successful in the exploit\n\n"
+
+sleep 10
+
+echo -e "DDos - \nBuffer - hidden file\nSQL - key hidden amongst text\n"
